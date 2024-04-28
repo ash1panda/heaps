3fe640e6d5e2daf16401259945fc3bf9636258d3
Ashutosh Panda <155313156+ash1panda@users.noreply.github.com>
2024-04-28 13:46:27 +0530

    CHanges

--- 04_Ques_1.cpp
+++ 04_Ques_1.cpp
@@ -4,10 +4,35 @@
 
 #include<bits/stdc++.h>
 #include<iostream>
+#include<queue>
 using namespace std;
+
+
+// First approach 
+// TC-O(log(n!)) ~ O(nlog(n))  
+// SC-O(n)
+int minHeapApp(vector<int> vec, int k){
+    priority_queue< int, vector<int>, greater<int> > pq;
+    for(int i=0; i<vec.size(); i++){
+        pq.push(vec[i]);
+    }
+    for(int i=0; i<k-1; i++){
+        pq.pop();
+    }
+    return pq.top();
+}
+
+
 
 int main()
 {
+    vector <int> vec = {10, 20, -4, 6, 18, 2, 105, 111};
+    int k=3;
+    cout << k << "th largest element is: " << minHeapApp(vec, k);
+
+    
+
+    
 
 return 0;
 }
