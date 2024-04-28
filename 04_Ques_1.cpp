51bd941ec0c0a72234f23f4178e10c93010c29bf
Ashutosh Panda <155313156+ash1panda@users.noreply.github.com>
2024-04-28 15:50:02 +0530

    Added 05 Ques

--- 04_Ques_1.cpp
+++ 04_Ques_1.cpp
@@ -1,14 +1,12 @@
 // QUESTION - FIND Kth SMALLEST ELEMENT IN AN ARRAY
 
-/*CODE by Ashutosh Panda*/
-
 #include<bits/stdc++.h>
 #include<iostream>
 #include<queue>
 using namespace std;
 
 
-// First approach 
+// First Approach --> BruteForce --> Less Optimal Approach --> Using MinHeap 
 // TC-O(log(n!)) ~ O(nlog(n))  
 // SC-O(n)
 int minHeapApp(vector<int> vec, int k){
@@ -22,13 +20,26 @@ int minHeapApp(vector<int> vec, int k){
     return pq.top();
 }
 
+// Second Approach --> Optimal Approach--> Using MaxHeap 
+// TC-O(nlog(k))  --> as we are putting k number of elements inside the pq --> and we are performing the operation n times -> so TC is nlog(k)
+// Additional Space - O(k) --> to make queue 
+int maxHeapApp(vector<int> vec, int k){
+    priority_queue<int>pq;
+    for(int i=0; i<vec.size(); i++){
+    pq.push(vec[i]);
+    if(pq.size()>k) pq.pop();
+    }
+    return pq.top();
+}
+
 
 
 int main()
 {
     vector <int> vec = {10, 20, -4, 6, 18, 2, 105, 111};
     int k=3;
-    cout << k << "th largest element is: " << minHeapApp(vec, k);
+    cout << k << "th largest element is: " << minHeapApp(vec, k) << endl;
+    cout << k << "th largest element is: " << maxHeapApp(vec, k);
 
     
 
---
+++ 05_Ques_2.cpp
@@ -0,0 +1,34 @@
+// QUESTION_2 - FIND THE Kth LARGEST ELEMENT IN AN ARRAY
+// LC - https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/1244043864/
+
+#include <bits/stdc++.h>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int findKthLargest(vector<int> &nums, int k)
+{
+    priority_queue<int, vector<int>, greater<int>> pq;  // We will be using minHeap
+    for (int i = 0; i < nums.size(); i++)
+    {
+        pq.push(nums[i]);
+        if (pq.size() > k)
+            pq.pop();
+    }
+    return pq.top();
+}
+
+int main()
+{
+    vector<int>nums = {3,2,1,5,6,4};
+    int k = 2;
+    cout << k << "th largest element is: "<<findKthLargest(nums, k);
+
+    return 0;
+}
+
+/*
+Time Complexity: O(nlog(k)) 
+Space Complexity: O(n);
+[Don't mind if I have not mentioned both of these :) ]
+*/
