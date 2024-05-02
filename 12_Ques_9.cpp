957f297c5514daeb9ea48433eca261f4cdd02bd3
Ashutosh Panda <155313156+ash1panda@users.noreply.github.com>
2024-05-02 09:04:30 +0530

    Added Ques9 and 10

--- 12_Ques_9.cpp
+++ 12_Ques_9.cpp
@@ -1,9 +1,32 @@
-/*CODE by Ashutosh Panda*/
+// QUESTION_9 LAST STONE WEIGHT
 
+// LC - https://leetcode.com/problems/last-stone-weight/description/
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
+int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
 
+        for(int i=0; i<stones.size(); i++){
+            maxHeap.push(stones[i]);
+        }
+
+        while(maxHeap.size() > 1){ 
+            int x = maxHeap.top();
+            maxHeap.pop();
+            int y = maxHeap.top();
+            maxHeap.pop();
+            if(x!=y){
+                maxHeap.push(abs(x-y));
+            }
+        }
+
+        if(maxHeap.empty()) return 0;
+        else{
+            return maxHeap.top();
+        }
+        
+    }
 int main()
 {
 
