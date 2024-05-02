eaa48ea54488be9c4af495cb7f4e299f9c8673bf
Ashutosh Panda <155313156+ash1panda@users.noreply.github.com>
2024-05-02 05:47:53 +0530

    Completed Ques 9 and 10

--- 09_Ques_6.cpp
+++ 09_Ques_6.cpp
@@ -4,7 +4,33 @@
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
-
+ vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> m;
+      
+        priority_queue<pair<int, int>> pq;
+        
+        vector<int> v;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
+        }
+        
+        for (auto it : m) {
+            pq.push({-1 * it.second, it.first});
+        }
+        
+        while(pq.size() > 0) {
+            int freq = -1 * pq.top().first;
+            int elem = pq.top().second;
+            
+            for (int i = 0; i < freq; i++) v.push_back(elem);
+            pq.pop();
+        }
+        
+        return v;
+    }
+    
 int main()
 {
 
--- 10_Ques_7.cpp
+++ 10_Ques_7.cpp
@@ -15,7 +15,23 @@ Remember -> You have to give the answer in sorted manner
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
+ vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+      priority_queue< pair<int,int>>pq;
+      for(int i=0; i<arr.size(); i++){
+        pq.push({abs(arr[i]-x), arr[i]});
+        if(pq.size()>k) pq.pop();
+      }
 
+      vector<int> ans;
+      while(!pq.empty()){
+        ans.push_back(pq.top().second);
+        pq.pop();
+      }
+
+      sort(ans.begin(), ans.end());
+
+      return ans;
+    }
 int main()
 {
 
