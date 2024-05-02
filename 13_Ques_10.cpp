2784ee58e39b0f32e231a80d0f09249748f903e6
Ashutosh Panda <155313156+ash1panda@users.noreply.github.com>
2024-05-02 09:55:29 +0530

    Added some question files

--- 13_Ques_10.cpp
+++ 13_Ques_10.cpp
@@ -1,37 +1,38 @@
 /*CODE by Ashutosh Panda*/
 
-
-
-#include<bits/stdc++.h>
-#include<iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-long long minCost(long long arr[], long long n) {
-        priority_queue< long long, vector<long long>, greater<long long> >pq;
-        for(long long i=0; i<n; i++){
-            pq.push(arr[i]);
-        }
-        
-        long long ans = 0;
-        
-        while(pq.size()>1){
-            long long x = pq.top();
-            pq.pop();
-            long long y = pq.top();
-            pq.pop();
-            ans += (x+y);
-            pq.push(x+y);
-        }
-        
-        return ans;
+long long minCost(long long arr[], long long n)
+{
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+    for (long long i = 0; i < n; i++)
+    {
+        pq.push(arr[i]);
     }
+
+    long long ans = 0;
+
+    while (pq.size() > 1)
+    {
+        long long x = pq.top();
+        pq.pop();
+        long long y = pq.top();
+        pq.pop();
+        ans += (x + y);
+        pq.push(x + y);
+    }
+
+    return ans;
+}
 int main()
 {
 
-return 0;
+    return 0;
 }
 
 /*
-Time Complexity: 
-Space Complexity: 
+Time Complexity:
+Space Complexity:
 [Don't mind if I have not mentioned both of these :) ]
 */
